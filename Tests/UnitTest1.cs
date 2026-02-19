a7ce0a81642124489cfac003095dd75689a963e9
3jamees <jamesiiking8@gmail.com>
2026-02-18 20:42:13 -0700

    Added Unit Test

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -3,8 +3,17 @@
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void ProductCollectiontest()
     {
+        ProductCollectiontest collection= new ProductCollectiontest();
+        Product product = new Product("Phone", 500m, "Smartphone");
 
+        collection.Add(product);
+
+        Assert.NotNull(collection.GetById(product.Id));
+
+        collection.Remove(product.Id);
+        
+        Assert.Null(collection.GetById(product.Id));
     }
 }
