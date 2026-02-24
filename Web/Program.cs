75438ada21727675f4f66b1837afdcf59b5dbc6d
GayGirl490 <astrid.cranney@students.snow.edu>
2026-02-23 19:56:30 -0700

    Resolved weird merge conflicts

--- Web/Program.cs
+++ Web/Program.cs
@@ -28,6 +28,7 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios,
+    // The default HSTS value is 30 days. You may want to change this for production scenarios,
     app.UseHsts();
 }
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
