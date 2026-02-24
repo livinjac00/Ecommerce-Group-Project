05a4d5662a4b778ffc8a66f3d2af789404c12fe7
GayGirl490 <astrid.cranney@students.snow.edu>
2026-02-23 19:57:10 -0700

    Removed duplicated comment

--- Web/Program.cs
+++ Web/Program.cs
@@ -28,7 +28,6 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios,
-    // The default HSTS value is 30 days. You may want to change this for production scenarios,
     app.UseHsts();
 }
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
